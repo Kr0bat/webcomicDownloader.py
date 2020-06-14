593a259c8a3e02bbe091efc698bd6cb8892993da
Karim Gueye <kgueye2000@gmail.com>
2020-06-14 14:53:26 -0400

    downloader.py committed. Support for other sites incoming

---
+++ downloader.py
@@ -0,0 +1,24 @@
+# downloader.py
+# Provides methods for downloading webcomics
+import requests, bs4
+from os.path import basename
+from pathlib import Path
+
+def xkcd():
+    siteURL = "https://xkcd.com"
+    site = requests.get(siteURL)
+    site.raise_for_status()
+    siteHTML = bs4.BeautifulSoup(site.text, 'html.parser')
+    imageURL = siteHTML.select('meta[property="og:image"]')[0].get('content')
+    title = siteHTML.select('meta[property="og:title"]')[0].get('content')
+    
+    image = requests.get(imageURL)
+    image.raise_for_status()
+    imageFile = open(f'{title}{Path(imageURL).suffix}', 'wb')
+    
+    for chunk in image.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+    
+xkcd()
+
