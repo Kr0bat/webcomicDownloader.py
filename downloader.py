d6003cb7f7b5d2eee6b4ddbb3c242014b0c0bddf
Karim Gueye <kgueye2000@gmail.com>
2020-06-15 12:08:22 -0400

    Added support for Mr. Lovenstein comics

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 # downloader.py
 # Provides methods for downloading webcomics
 # This line is pointless
-import requests, bs4
+import requests, bs4, re
 from pathlib import Path
 
 def xkcd():
@@ -21,12 +21,14 @@ def xkcd():
     imageFile.close()
     
 def lovenstein():
+    nameFormat = re.compile(r'Mr. Lovenstein \| ')
     siteURL = "https://www.mrlovenstein.com"
     site = requests.get(siteURL, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
     site.raise_for_status()
     siteHTML = bs4.BeautifulSoup(site.text, 'html.parser')
-    imageURL = siteHTML.select('meta[property="og:image"]')[0].get('content')
-    title = siteHTML.select('meta[property="og:title"]')[0].get('content')
+    imageURL = siteURL + siteHTML.select('div > img')[0].get('src')
+    title =  siteHTML.select('title')[0].text
+    title = nameFormat.sub('', title)
     
     image = requests.get(imageURL)
     image.raise_for_status()
@@ -35,4 +37,6 @@ def lovenstein():
     for chunk in image.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
+    
+lovenstein()
 
